ef3f15ff489040a4eede97680c3bc793f313353b
Mikhail Barsukou <mikhail_barsukou@epam.com>
2017-06-13 18:31:14 +0300

    removed unnecessary stuff

--- SecondWeb/src/com/study/sample/logic/Logic.java
+++
@@ -1,8 +0,0 @@
-package com.study.sample.logic;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public abstract class Logic {
-	protected static Logger logger = LogManager.getLogger(Logic.class);
-}
